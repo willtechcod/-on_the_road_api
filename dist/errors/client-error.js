1cdd77948cfb0654f8516a366c469c67bbe72c5a
willtechcod <dev.pereira2019@gmail.com>
2024-07-14 07:15:09 -0300

    chore: correct project

--- dist/errors/client-error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ClientError = void 0;
-class ClientError extends Error {
-}
-exports.ClientError = ClientError;
